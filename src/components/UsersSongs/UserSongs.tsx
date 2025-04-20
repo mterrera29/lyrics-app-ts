4790be8f44791baf98db9322692952c62c3ce86b
mterrera29 <matiasterrera29@gmail.com>
2025-04-20 16:23:39 -0300

    styles

--- src/components/UsersSongs/UserSongs.tsx
+++ src/components/UsersSongs/UserSongs.tsx
@@ -20,6 +20,7 @@ export default function UserSongs({ songs }: UserSongsProps) {
     isOpen: false,
     selectedSong: null,
   });
+  const [sortOrder, setSortOrder] = useState('desc');
 
   useEffect(() => {
     const filtered: Song[] = songs.filter((song) => {
@@ -35,10 +36,22 @@ export default function UserSongs({ songs }: UserSongsProps) {
       return matchesAuthor && matchesGenre && matchesSearch;
     });
 
-    setFilteredSongs(filtered);
-  }, [selectedAuthor, selectedGenre, searchQuery, songs]);
+    const sorted = filtered.sort((a, b) =>
+      sortOrder === 'asc'
+        ? a.title.localeCompare(b.title)
+        : b.title.localeCompare(a.title)
+    );
+
+    setFilteredSongs(sorted);
+  }, [selectedAuthor, selectedGenre, searchQuery, songs, sortOrder]);
+
+  useEffect(() => {
+    setSelectedAuthor('');
+    setSelectedGenre('');
+  }, [selectedButton]);
+
   return (
-    <div style={{ width: '500px' }}>
+    <div className={styles.container}>
       <section
         style={{
           display: 'flex',
@@ -146,6 +159,52 @@ export default function UserSongs({ songs }: UserSongsProps) {
             />
           </div>
         )}
+        <div
+          onClick={() =>
+            setSortOrder((prev) => {
+              if (prev === 'asc') {
+                return 'des';
+              } else {
+                return 'asc';
+              }
+            })
+          }
+        >
+          <div className={styles.sort}>
+            {sortOrder === 'asc' ? (
+              <svg
+                xmlns='http://www.w3.org/2000/svg'
+                width='25'
+                height='25'
+                fill='currentColor'
+                className={styles.sortIcon}
+                viewBox='0 0 16 16'
+              >
+                <path
+                  fill-rule='evenodd'
+                  d='M10.082 5.629 9.664 7H8.598l1.789-5.332h1.234L13.402 7h-1.12l-.419-1.371zm1.57-.785L11 2.687h-.047l-.652 2.157z'
+                />
+                <path d='M12.96 14H9.028v-.691l2.579-3.72v-.054H9.098v-.867h3.785v.691l-2.567 3.72v.054h2.645zM4.5 2.5a.5.5 0 0 0-1 0v9.793l-1.146-1.147a.5.5 0 0 0-.708.708l2 1.999.007.007a.497.497 0 0 0 .7-.006l2-2a.5.5 0 0 0-.707-.708L4.5 12.293z' />
+              </svg>
+            ) : (
+              <svg
+                xmlns='http://www.w3.org/2000/svg'
+                width='25'
+                height='25'
+                fill='currentColor'
+                className={styles.sortIcon}
+                viewBox='0 0 16 16'
+              >
+                <path d='M12.96 7H9.028v-.691l2.579-3.72v-.054H9.098v-.867h3.785v.691l-2.567 3.72v.054h2.645z' />
+                <path
+                  fill-rule='evenodd'
+                  d='M10.082 12.629 9.664 14H8.598l1.789-5.332h1.234L13.402 14h-1.12l-.419-1.371zm1.57-.785L11 9.688h-.047l-.652 2.156z'
+                />
+                <path d='M4.5 2.5a.5.5 0 0 0-1 0v9.793l-1.146-1.147a.5.5 0 0 0-.708.708l2 1.999.007.007a.497.497 0 0 0 .7-.006l2-2a.5.5 0 0 0-.707-.708L4.5 12.293z' />
+              </svg>
+            )}
+          </div>
+        </div>
       </div>
       {songs.length === 0 ? (
         <p>No hay canciones aún.</p>
