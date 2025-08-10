0326769654a577ca5786827c59ea388d18b1f58a
mterrera29 <matiasterrera29@gmail.com>
2025-08-10 19:55:53 -0300

    fetch

--- vite.config.ts
+++ vite.config.ts
@@ -9,25 +9,41 @@ export default defineConfig({
     react(),
     svgr(),
     VitePWA({
-      registerType: 'autoUpdate',
-      includeAssets: ['favicon.svg'],
+      registerType: 'autoUpdate', // Configura el Service Worker para actualizarse automáticamente.
       manifest: {
-        name: 'Cancionero',
-        short_name: 'Cancionero',
-        description: 'Una aplicación progresiva creada con React y Vite',
-        display: 'standalone',
-        background_color: '#ffffff',
-        theme_color: '#4f46e5',
+        name: 'Cancionero', // Nombre completo de la aplicación.
+        short_name: 'Cancionero', // Nombre corto que aparece en la pantalla de inicio.
+        description: 'Una Progressive Web App creada con React y Vite', // Breve descripción de la aplicación.
+        theme_color: '#ffffff', // Color del tema que se muestra en la barra de herramientas del navegador.
+        background_color: '#000000', // Color de fondo de la pantalla de carga inicial.
+        display: 'standalone', // Modo de visualización: "standalone" simula una app nativa.
+        orientation: 'portrait', // Orientación preferida de la aplicación.
+        start_url: '/', // URL inicial al abrir la aplicación.
         icons: [
           {
-            src: 'public/icon.png',
-            sizes: '192x192',
-            type: 'image/png',
+            src: '/icon.png', // Ruta del icono de 192x192 píxeles.
+            sizes: '192x192', // Tamaño del icono.
+            type: 'image/png', // Tipo de archivo del icono.
           },
           {
-            src: 'public/icon.png',
-            sizes: '512x512',
-            type: 'image/png',
+            src: '/icon.png', // Ruta del icono de 512x512 píxeles.
+            sizes: '512x512', // Tamaño del icono.
+            type: 'image/png', // Tipo de archivo del icono.
+          },
+        ],
+      },
+      workbox: {
+        runtimeCaching: [
+          {
+            urlPattern: /^http:\/\/localhost:5173\/.*$/, // Patrón para manejar las solicitudes del localhost.
+            handler: 'NetworkFirst', // Intenta primero la red y luego la caché.
+            options: {
+              cacheName: 'local-cache', // Nombre de la caché para estas solicitudes.
+              expiration: {
+                maxEntries: 50, // Máximo de recursos en caché.
+                maxAgeSeconds: 86400, // Duración máxima en la caché (1 día).
+              },
+            },
           },
         ],
       },
